4af611d8eb1c0935d83058fac23fb9430e1368aa
sangam14 <smbiradar14@gmail.com>
2025-04-23 18:23:36 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://aidatafoundation.github.io',
+  base: 'https://cloudsecuritycorner.com',
   plugins: [react()]
 })
